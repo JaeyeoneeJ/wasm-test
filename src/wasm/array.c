31afabf7caaea72c1d2460eb55132d57ca8ce85c
JaeyeoneeJ <5ikve@naver.com>
2024-02-23 13:56:12 +0900

    mjs, wasm 파일명 및 경로 변경

--- src/wasm/array.c
+++ src/wasm/array.c
@@ -14,7 +14,7 @@ EMSCRIPTEN_KEEPALIVE int* array(int size, int a, int b) {
     int* result = (int*)malloc(size * sizeof(int));  // 배열 크기 정보를 위한 추가 공간 확보
 
     if(result == NULL) {
-      fprintf(stderr, "메모리 할당에 실패했습니다.\n");
+        fprintf(stderr, "메모리 할당에 실패했습니다.\n");
         exit(1);
     }
 
