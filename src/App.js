1e1738694bbba9bd8a03b792ca7693e427dc015a
JaeyeoneeJ <5ikve@naver.com>
2024-02-23 16:01:00 +0900

    JS / WASM 시간 측정

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import WasmModule from "./components/WasmModule";
 import JsModule from "./components/JsModule";
 
 function App() {
-  const [selectMode, setSelectMode] = useState("JS");
+  const [selectMode, setSelectMode] = useState("WASM");
   return (
     <div className="App">
       <h2>Wasm Module Load Test</h2>
       <button onClick={() => setSelectMode("JS")}>C to JS</button>
       <button onClick={() => setSelectMode("WASM")}>C to WASM</button>
-      <div>{selectMode === "JS" ? <JsModule /> : <WasmModule />}</div>
+      <JsModule /> <WasmModule />
     </div>
   );
 }
--- src/components/WasmModule.jsx
+++ src/components/WasmModule.jsx
@@ -1,74 +1,102 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import wasmModuleFile from "../wasm/wasmModule.wasm";
 
-let wasmModule = null;
+const size = 100000;
+const timeset = 20;
 
 const loadWasmModule = async () => {
-  if (!wasmModule) {
-    try {
-      const importObject = {
-        wasi_snapshot_preview1: {
-          proc_exit: () => {},
-          fd_close: () => {},
-          fd_write: () => {},
-          fd_seek: () => {},
-        },
-      };
+  try {
+    const importObject = {
+      wasi_snapshot_preview1: {
+        proc_exit: () => {},
+        fd_close: () => {},
+        fd_write: () => {},
+        fd_seek: () => {},
+      },
+    };
 
-      const response = await fetch(wasmModuleFile);
-      console.log("jjy response", response);
-      const buffer = await response.arrayBuffer();
-      console.log("jjy buffer", buffer);
-      const result = await WebAssembly.instantiate(buffer, importObject);
+    const response = await fetch(wasmModuleFile);
+    const buffer = await response.arrayBuffer();
+    const result = await WebAssembly.instantiate(buffer, importObject);
 
-      wasmModule = result.instance.exports;
-    } catch (e) {
-      console.warn("jjy loadWasmModule error", e);
-    }
+    return result.instance.exports;
+  } catch (e) {
+    console.warn("loadWasmModule error", e);
   }
-
-  return wasmModule;
 };
 
 const WasmModule = () => {
+  const [module, setModule] = useState();
+
+  const cTime = useMemo(() => {
+    if (module) {
+      const time = [];
+      const arrayFunc = module.array;
+
+      for (let i = 0; i < timeset; i++) {
+        const startTime = performance.now();
+
+        const resultPtr = arrayFunc(size, 2, 3);
+        // console.log("jjy resultPtr c", resultPtr);
+        const dataArray = new Int32Array(module.memory.buffer, resultPtr, size);
+        // console.log("jjy c dataArray", dataArray);
+
+        const endTime = performance.now();
+        const duringTime = endTime - startTime;
+
+        time.push(duringTime);
+        console.log("jjy c duringTime", duringTime);
+      }
+
+      if (time.length > 0) {
+        const sum = time.reduce((acc, value) => acc + value, 0);
+        const average = sum / time.length;
+        console.log("jjy c time", time);
+        console.log("jjy c average", sum / time.length);
+        return average;
+      }
+      module.free(arrayFunc);
+    }
+  }, [module, timeset]);
+
+  const jsTime = useMemo(() => {
+    const time = [];
+
+    for (let i = 0; i < timeset; i++) {
+      const startTime = performance.now();
+      const obj = [];
+      for (let i = 0; i < size; i++) {
+        obj.push(2 * 3 * i);
+      }
+      // console.log("jjy js dataArray", obj);
+      const endTime = performance.now();
+      const duringTime = endTime - startTime;
+      time.push(duringTime);
+      console.log("jjy js duringTime", duringTime);
+    }
+    if (time.length > 0) {
+      const sum = time.reduce((acc, value) => acc + value, 0);
+      console.log("jjy js time", time);
+      console.log("jjy js average", sum / time.length);
+      const average = sum / time.length;
+      return average;
+    }
+  }, [timeset]);
+
   useEffect(() => {
     const initializeWasm = async () => {
-      try {
-        const wasmModule = await loadWasmModule();
-        const size = 5;
-        const a = 2;
-        const b = 3;
-        console.log("jjy wasmModule", wasmModule);
-        // const addFunc = wasmModule.add;
-        const arrayFunc = wasmModule.array;
-        // console.log("jjy addFunc", addFunc(a, b));
-
-        const resultArrayPointer = arrayFunc(size, a, b);
-
-        console.log("jjy arrayFunc", arrayFunc(size, a, b));
-        const resultArray = new Int32Array(
-          wasmModule.memory.buffer,
-          resultArrayPointer,
-          size
-        );
-        console.log("jjy arrayFunc", resultArray);
-        // 메모리 해제
-        // wasmModule._free(addFunc);
-        wasmModule.free(arrayFunc);
-        return wasmModule;
-      } catch (e) {
-        console.log("jjy initializeWasm error", e);
-      }
+      const wasmModule = await loadWasmModule();
+      setModule(wasmModule);
     };
-    console.log("jjy ?", initializeWasm());
+    initializeWasm();
   }, []);
 
   return (
     <div>
       <h3>C to WASM Duration time</h3>
-      {/* <p>c = {cTime}</p>
+      <p>c = {cTime}</p>
       <p>js = {jsTime}</p>
-      <p>js / c = {jsTime / cTime}</p> */}
+      <p>js / c = {jsTime / cTime}</p>
     </div>
   );
 };
