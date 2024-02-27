8760a08edd23e4b55f3014f677bb8bd6744ab76f
JaeyeoneeJ <5ikve@naver.com>
2024-02-27 09:49:39 +0900

    data 인자 값 table 표시

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import WasmModule from "./components/WasmModule";
 
-const data = { size: 100000, timeset: 20, a: 12, b: 13 };
+const data = { size: 300000, timeset: 10, a: 2016, b: 50817 };
 
 function App() {
   return (
     <div className="App">
       <h2>Wasm Module Load Test</h2>
-      {Object.keys(data).map((key, index) => (
-        <div key={index}>
-          <span>{key}: </span>
-          <span>{data[key]}</span>
-        </div>
-      ))}
+      <table>
+        <tbody>
+          {Object.keys(data).map((key, index) => (
+            <tr key={index}>
+              <td>{key}: </td>
+              <td>{data[key]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
       <WasmModule data={data} />
     </div>
   );
